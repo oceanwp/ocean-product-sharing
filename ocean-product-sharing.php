4c240d1d520f80f37836bfaef071c8eba3400c44
Mohammad Taghi Elmi <mohammad.taghi.elmi@gmail.com>
2021-06-13 16:28:18 +0430

    first commit

--- ocean-product-sharing.php
+++ ocean-product-sharing.php
@@ -1,15 +1,15 @@
 <?php
 /**
- * Plugin Name:			Ocean Product Sharing
- * Plugin URI:			https://oceanwp.org/extension/ocean-product-sharing/
- * Description:			A simple plugin to add social share buttons to your product page, compatible with WooCommerce and Easy Digital Downloads.
- * Version:				1.1.2
- * Author:				OceanWP
- * Author URI:			https://oceanwp.org/
- * Requires at least:	5.3
- * Tested up to:		5.7.2
+ * Plugin Name:         Ocean Product Sharing
+ * Plugin URI:          https://oceanwp.org/extension/ocean-product-sharing/
+ * Description:         A simple plugin to add social share buttons to your product page, compatible with WooCommerce and Easy Digital Downloads.
+ * Version:             2.0.0
+ * Author:              OceanWP
+ * Author URI:          https://oceanwp.org/
+ * Requires at least:   5.3
+ * Tested up to:        5.7.2
  * WC requires at least:3.0
- * WC tested up to: 	5.4.0
+ * WC tested up to:     5.4.0
  *
  * Text Domain: ocean-product-sharing
  * Domain Path: /languages
@@ -40,21 +40,23 @@ Ocean_Product_Sharing();
  * Main Ocean_Product_Sharing Class
  *
  * @class Ocean_Product_Sharing
- * @version	1.0.0
+ * @version 1.0.0
  * @since 1.0.0
- * @package	Ocean_Product_Sharing
+ * @package Ocean_Product_Sharing
  */
 final class Ocean_Product_Sharing {
 	/**
 	 * Ocean_Product_Sharing The single instance of Ocean_Product_Sharing.
-	 * @var 	object
+	 *
+	 * @var     object
 	 * @access  private
-	 * @since 	1.0.0
+	 * @since   1.0.0
 	 */
 	private static $_instance = null;
 
 	/**
 	 * The token.
+	 *
 	 * @var     string
 	 * @access  public
 	 * @since   1.0.0
@@ -63,6 +65,7 @@ final class Ocean_Product_Sharing {
 
 	/**
 	 * The version number.
+	 *
 	 * @var     string
 	 * @access  public
 	 * @since   1.0.0
@@ -72,6 +75,7 @@ final class Ocean_Product_Sharing {
 	// Admin - Start
 	/**
 	 * The admin object.
+	 *
 	 * @var     object
 	 * @access  public
 	 * @since   1.0.0
@@ -80,15 +84,16 @@ final class Ocean_Product_Sharing {
 
 	/**
 	 * Constructor function.
+	 *
 	 * @access  public
 	 * @since   1.0.0
 	 * @return  void
 	 */
 	public function __construct( $widget_areas = array() ) {
-		$this->token 			= 'ocean-product-sharing';
-		$this->plugin_url 		= plugin_dir_url( __FILE__ );
-		$this->plugin_path 		= plugin_dir_path( __FILE__ );
-		$this->version 			= '1.1.2';
+		$this->token       = 'ocean-product-sharing';
+		$this->plugin_url  = plugin_dir_url( __FILE__ );
+		$this->plugin_path = plugin_dir_path( __FILE__ );
+		$this->version     = '1.1.2';
 
 		register_activation_hook( __FILE__, array( $this, 'install' ) );
 
@@ -108,13 +113,15 @@ final class Ocean_Product_Sharing {
 	 * @return Main Ocean_Product_Sharing instance
 	 */
 	public static function instance() {
-		if ( is_null( self::$_instance ) )
+		if ( is_null( self::$_instance ) ) {
 			self::$_instance = new self();
+		}
 		return self::$_instance;
 	} // End instance()
 
 	/**
 	 * Load the localisation file.
+	 *
 	 * @access  public
 	 * @since   1.0.0
 	 * @return  void
@@ -144,6 +151,7 @@ final class Ocean_Product_Sharing {
 	/**
 	 * Installation.
 	 * Runs on activation. Logs the version number and assigns a notice message to a WordPress option.
+	 *
 	 * @access  public
 	 * @since   1.0.0
 	 * @return  void
@@ -154,6 +162,7 @@ final class Ocean_Product_Sharing {
 
 	/**
 	 * Log the plugin version number.
+	 *
 	 * @access  private
 	 * @since   1.0.0
 	 * @return  void
@@ -166,13 +175,14 @@ final class Ocean_Product_Sharing {
 	/**
 	 * Setup all the things.
 	 * Only executes if OceanWP or a child theme using OceanWP as a parent is active and the extension specific filter returns true.
+	 *
 	 * @return void
 	 */
 	public function ops_setup() {
 		$theme = wp_get_theme();
 
 		if ( 'OceanWP' == $theme->name || 'oceanwp' == $theme->template ) {
-			require_once( $this->plugin_path .'/includes/helpers.php' );
+			require_once $this->plugin_path . '/includes/helpers.php';
 			add_action( 'customize_register', array( $this, 'ops_customizer_register' ) );
 			add_action( 'customize_preview_init', array( $this, 'ops_customize_preview_js' ) );
 			add_action( 'wp_enqueue_scripts', array( $this, 'ops_get_style' ), 999 );
@@ -184,86 +194,126 @@ final class Ocean_Product_Sharing {
 
 	/**
 	 * Customizer Controls and settings
+	 *
 	 * @param WP_Customize_Manager $wp_customize Theme Customizer object.
 	 */
 	public function ops_customizer_register( $wp_customize ) {
 
 		/**
-	     * Add a new section
-	     */
-		$wp_customize->add_section( 'ops_product_sharing_section' , array(
-		    'title'      	=> esc_html__( 'Product Sharing', 'ocean-product-sharing' ),
-		    'priority'   	=> 210,
-		) );
+		 * Add a new section
+		 */
+		$wp_customize->add_section(
+			'ops_product_sharing_section',
+			array(
+				'title'    => esc_html__( 'Product Sharing', 'ocean-product-sharing' ),
+				'priority' => 210,
+			)
+		);
 
 		/**
-	     * Sharing sites
-	     */
-        $wp_customize->add_setting( 'ops_product_sharing_sites', array(
-			'default'           	=> array( 'twitter', 'facebook', 'pinterest', 'email' ),
-			'sanitize_callback' 	=> 'oceanwp_sanitize_multi_choices',
-		) );
+		 * Sharing sites
+		 */
+		$wp_customize->add_setting(
+			'ops_product_sharing_sites',
+			array(
+				'default'           => array( 'twitter', 'facebook', 'pinterest', 'email' ),
+				'sanitize_callback' => 'oceanwp_sanitize_multi_choices',
+			)
+		);
 
-		$wp_customize->add_control( new OceanWP_Customizer_Sortable_Control( $wp_customize, 'ops_product_sharing_sites', array(
-			'label'	   				=> esc_html__( 'Sharing Buttons', 'ocean-product-sharing' ),
-			'section'  				=> 'ops_product_sharing_section',
-			'settings' 				=> 'ops_product_sharing_sites',
-			'priority' 				=> 10,
-			'choices' 				=> array(
-				'twitter'  		=> 'Twitter',
-				'facebook' 		=> 'Facebook',
-				'pinterest' 	=> 'Pinterest',
-				'email' 		=> 'Mail',
-			),
-		) ) );
-
-		/**
-	     * Borders color
-	     */
-        $wp_customize->add_setting( 'ops_product_sharing_borders_color', array(
-			'default'			=> '#e9e9e9',
-			'transport'			=> 'postMessage',
-			'sanitize_callback' => 'oceanwp_sanitize_color',
-		) );
-
-		$wp_customize->add_control( new OceanWP_Customizer_Color_Control( $wp_customize, 'ops_product_sharing_borders_color', array(
-			'label'			=> esc_html__( 'Borders Color', 'ocean-product-sharing' ),
-			'section'		=> 'ops_product_sharing_section',
-			'settings'		=> 'ops_product_sharing_borders_color',
-			'priority'		=> 10,
-		) ) );
+		$wp_customize->add_control(
+			new OceanWP_Customizer_Sortable_Control(
+				$wp_customize,
+				'ops_product_sharing_sites',
+				array(
+					'label'    => esc_html__( 'Sharing Buttons', 'ocean-product-sharing' ),
+					'section'  => 'ops_product_sharing_section',
+					'settings' => 'ops_product_sharing_sites',
+					'priority' => 10,
+					'choices'  => array(
+						'twitter'   => 'Twitter',
+						'facebook'  => 'Facebook',
+						'pinterest' => 'Pinterest',
+						'email'     => 'Mail',
+					),
+				)
+			)
+		);
 
 		/**
-	     * Icons background color
-	     */
-        $wp_customize->add_setting( 'ops_product_sharing_icons_bg', array(
-			'default'			=> '#333333',
-			'transport'			=> 'postMessage',
-			'sanitize_callback' => 'oceanwp_sanitize_color',
-		) );
+		 * Borders color
+		 */
+		$wp_customize->add_setting(
+			'ops_product_sharing_borders_color',
+			array(
+				'default'           => '#e9e9e9',
+				'transport'         => 'postMessage',
+				'sanitize_callback' => 'oceanwp_sanitize_color',
+			)
+		);
 
-		$wp_customize->add_control( new OceanWP_Customizer_Color_Control( $wp_customize, 'ops_product_sharing_icons_bg', array(
-			'label'			=> esc_html__( 'Icons Background Color', 'ocean-product-sharing' ),
-			'section'		=> 'ops_product_sharing_section',
-			'settings'		=> 'ops_product_sharing_icons_bg',
-			'priority'		=> 10,
-		) ) );
+		$wp_customize->add_control(
+			new OceanWP_Customizer_Color_Control(
+				$wp_customize,
+				'ops_product_sharing_borders_color',
+				array(
+					'label'    => esc_html__( 'Borders Color', 'ocean-product-sharing' ),
+					'section'  => 'ops_product_sharing_section',
+					'settings' => 'ops_product_sharing_borders_color',
+					'priority' => 10,
+				)
+			)
+		);
 
 		/**
-	     * Icons color
-	     */
-        $wp_customize->add_setting( 'ops_product_sharing_icons_color', array(
-			'default'			=> '#ffffff',
-			'transport'			=> 'postMessage',
-			'sanitize_callback' => 'oceanwp_sanitize_color',
-		) );
+		 * Icons background color
+		 */
+		$wp_customize->add_setting(
+			'ops_product_sharing_icons_bg',
+			array(
+				'default'           => '#333333',
+				'transport'         => 'postMessage',
+				'sanitize_callback' => 'oceanwp_sanitize_color',
+			)
+		);
 
-		$wp_customize->add_control( new OceanWP_Customizer_Color_Control( $wp_customize, 'ops_product_sharing_icons_color', array(
-			'label'			=> esc_html__( 'Icons Color', 'ocean-product-sharing' ),
-			'section'		=> 'ops_product_sharing_section',
-			'settings'		=> 'ops_product_sharing_icons_color',
-			'priority'		=> 10,
-		) ) );
+		$wp_customize->add_control(
+			new OceanWP_Customizer_Color_Control(
+				$wp_customize,
+				'ops_product_sharing_icons_bg',
+				array(
+					'label'    => esc_html__( 'Icons Background Color', 'ocean-product-sharing' ),
+					'section'  => 'ops_product_sharing_section',
+					'settings' => 'ops_product_sharing_icons_bg',
+					'priority' => 10,
+				)
+			)
+		);
+
+		/**
+		 * Icons color
+		 */
+		$wp_customize->add_setting(
+			'ops_product_sharing_icons_color',
+			array(
+				'default'           => '#ffffff',
+				'transport'         => 'postMessage',
+				'sanitize_callback' => 'oceanwp_sanitize_color',
+			)
+		);
+
+		$wp_customize->add_control(
+			new OceanWP_Customizer_Color_Control(
+				$wp_customize,
+				'ops_product_sharing_icons_color',
+				array(
+					'label'    => esc_html__( 'Icons Color', 'ocean-product-sharing' ),
+					'section'  => 'ops_product_sharing_section',
+					'settings' => 'ops_product_sharing_icons_color',
+					'priority' => 10,
+				)
+			)
+		);
 
 	}
 
@@ -276,6 +326,7 @@ final class Ocean_Product_Sharing {
 
 	/**
 	 * Enqueue style.
+	 *
 	 * @since   1.0.0
 	 */
 	public function ops_get_style() {
@@ -298,7 +349,7 @@ final class Ocean_Product_Sharing {
 	 */
 	public function ops_product_share() {
 
-		$file 		= $this->plugin_path . 'template/product-share.php';
+		$file       = $this->plugin_path . 'template/product-share.php';
 		$theme_file = get_stylesheet_directory() . '/templates/extra/product-share.php';
 
 		if ( file_exists( $theme_file ) ) {
@@ -317,26 +368,26 @@ final class Ocean_Product_Sharing {
 	public function ops_head_css( $output ) {
 
 		// Global vars
-		$product_sharing_borders 		= get_theme_mod( 'ops_product_sharing_borders_color', '#e9e9e9' );
-		$product_sharing_icons_bg 		= get_theme_mod( 'ops_product_sharing_icons_bg', '#333333' );
-		$product_sharing_icons_color 	= get_theme_mod( 'ops_product_sharing_icons_color', '#ffffff' );
+		$product_sharing_borders     = get_theme_mod( 'ops_product_sharing_borders_color', '#e9e9e9' );
+		$product_sharing_icons_bg    = get_theme_mod( 'ops_product_sharing_icons_bg', '#333333' );
+		$product_sharing_icons_color = get_theme_mod( 'ops_product_sharing_icons_color', '#ffffff' );
 
 		// Define css var
 		$css = '';
 
 		// Add borders color
 		if ( ! empty( $product_sharing_borders ) && '#e9e9e9' != $product_sharing_borders ) {
-			$css .= '.oew-product-share,.oew-product-share ul li{border-color:'. $product_sharing_borders .';}';
+			$css .= '.oew-product-share,.oew-product-share ul li{border-color:' . $product_sharing_borders . ';}';
 		}
 
 		// Add icon background
 		if ( ! empty( $product_sharing_icons_bg ) && '#333333' != $product_sharing_icons_bg ) {
-			$css .= '.oew-product-share ul li a .ops-icon-wrap{background-color:'. $product_sharing_icons_bg .';}';
+			$css .= '.oew-product-share ul li a .ops-icon-wrap{background-color:' . $product_sharing_icons_bg . ';}';
 		}
 
 		// Add icon color
 		if ( ! empty( $product_sharing_icons_color ) && '#ffffff' != $product_sharing_icons_color ) {
-			$css .= '.oew-product-share ul li a .ops-icon-wrap .ops-icon{fill:'. $product_sharing_icons_color .';}';
+			$css .= '.oew-product-share ul li a .ops-icon-wrap .ops-icon{fill:' . $product_sharing_icons_color . ';}';
 		}
 
 		// Return CSS
@@ -351,49 +402,51 @@ final class Ocean_Product_Sharing {
 
 } // End Class
 
-#--------------------------------------------------------------------------------
-#region Freemius
-#--------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------
+// region Freemius
+// --------------------------------------------------------------------------------
 
 if ( ! function_exists( 'ocean_product_sharing_fs' ) ) {
-    // Create a helper function for easy SDK access.
-    function ocean_product_sharing_fs() {
-	    global $ocean_product_sharing_fs;
+	// Create a helper function for easy SDK access.
+	function ocean_product_sharing_fs() {
+		global $ocean_product_sharing_fs;
 
-	    if ( ! isset( $ocean_product_sharing_fs ) ) {
-		    $ocean_product_sharing_fs = OceanWP_EDD_Addon_Migration::instance( 'ocean_product_sharing_fs' )->init_sdk( array(
-			    'id'              => '3809',
-			    'slug'            => 'ocean-product-sharing',
-			    'public_key'      => 'pk_e8cc3b7980be98f86dc7286572cc0',
-			    'is_premium'      => false,
-			    'is_premium_only' => false,
-			    'has_paid_plans'  => false,
-		    ) );
-	    }
+		if ( ! isset( $ocean_product_sharing_fs ) ) {
+			$ocean_product_sharing_fs = OceanWP_EDD_Addon_Migration::instance( 'ocean_product_sharing_fs' )->init_sdk(
+				array(
+					'id'              => '3809',
+					'slug'            => 'ocean-product-sharing',
+					'public_key'      => 'pk_e8cc3b7980be98f86dc7286572cc0',
+					'is_premium'      => false,
+					'is_premium_only' => false,
+					'has_paid_plans'  => false,
+				)
+			);
+		}
 
-	    return $ocean_product_sharing_fs;
-    }
+		return $ocean_product_sharing_fs;
+	}
 
-    function ocean_product_sharing_fs_addon_init() {
-        if ( class_exists( 'Ocean_Extra' ) ) {
-            OceanWP_EDD_Addon_Migration::instance( 'ocean_product_sharing_fs' )->init();
-        }
-    }
+	function ocean_product_sharing_fs_addon_init() {
+		if ( class_exists( 'Ocean_Extra' ) ) {
+			OceanWP_EDD_Addon_Migration::instance( 'ocean_product_sharing_fs' )->init();
+		}
+	}
 
-    if ( 0 == did_action( 'owp_fs_loaded' ) ) {
-        // Init add-on only after parent theme was loaded.
-        add_action( 'owp_fs_loaded', 'ocean_product_sharing_fs_addon_init', 15 );
-    } else {
-        if ( class_exists( 'Ocean_Extra' ) ) {
-            /**
-             * This makes sure that if the theme was already loaded
-             * before the plugin, it will run Freemius right away.
-             *
-             * This is crucial for the plugin's activation hook.
-             */
-            ocean_product_sharing_fs_addon_init();
-        }
-    }
+	if ( 0 == did_action( 'owp_fs_loaded' ) ) {
+		// Init add-on only after parent theme was loaded.
+		add_action( 'owp_fs_loaded', 'ocean_product_sharing_fs_addon_init', 15 );
+	} else {
+		if ( class_exists( 'Ocean_Extra' ) ) {
+			/**
+			 * This makes sure that if the theme was already loaded
+			 * before the plugin, it will run Freemius right away.
+			 *
+			 * This is crucial for the plugin's activation hook.
+			 */
+			ocean_product_sharing_fs_addon_init();
+		}
+	}
 }
 
-#endregion
+// endregion
