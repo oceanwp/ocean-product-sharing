0bee74008dc6a4cc79c07375bef806b175885cb5
markhollo <samozatrash@yahoo.com>
2024-08-30 12:20:08 +0200

    label changes / help link

--- includes/options.php
+++ includes/options.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * OceanWP Customizer Class
+ * OceanWP Customizer Class: Product Sharing
  *
  * @package OceanWP WordPress theme
  */
@@ -15,19 +15,26 @@ if ( ! defined( 'ABSPATH' ) ) {
 function ops_customizer_options() {
 
 	$options = [
-		'title' => __( 'Product Sharing', 'ocean-product-sharing' ),
+		'title'    => __( 'Product Sharing', 'ocean-product-sharing' ),
 		'priority' => 16,
-		'options' => [
+		'options'  => [
+			'ops_spacer_before_sharing_options' => [
+				'type'      => 'ocean-spacer',
+				'section'   => 'ocean_product_sharing_settings',
+				'transport' => 'postMessage',
+				'priority'  => 10,
+			],
+
 			'ops_product_sharing_sites' => [
-				'label' => esc_html__( 'Sharing Buttons', 'ocean-product-sharing' ),
-				'type' => 'ocean-sortable',
-				'section'  => 'ocean_product_sharing_settings',
+				'label'     => esc_html__( 'Sharing Buttons', 'ocean-product-sharing' ),
+				'type'      => 'ocean-sortable',
+				'section'   => 'ocean_product_sharing_settings',
 				'transport' => 'refresh',
-				'priority' => 10,
-				'default'  => ['twitter', 'facebook', 'pinterest', 'email'],
+				'priority'  => 10,
+				'default'   => ['twitter', 'facebook', 'pinterest', 'email'],
 				'hideLabel' => false,
-				'choices' => [
-					'twitter'   => 'Twitter',
+				'choices'   => [
+					'twitter'   => 'X (ex Twitter)',
 					'facebook'  => 'Facebook',
 					'pinterest' => 'Pinterest',
 					'email'     => 'Mail',
@@ -35,33 +42,33 @@ function ops_customizer_options() {
 			],
 
 			'ops_divider_after_product_share_sites' => [
-				'type' => 'ocean-divider',
-				'section' => 'ocean_product_sharing_settings',
+				'type'      => 'ocean-divider',
+				'section'   => 'ocean_product_sharing_settings',
 				'transport' => 'postMessage',
-				'priority' => 10,
-				'top' => 1,
-				'bottom' => 1
+				'priority'  => 10,
+				'top'       => 20,
+				'bottom'    => 25,
 			],
 
 			'ops_product_sharing_borders_color' => [
-				'type' => 'ocean-color',
-				'label' => esc_html__( 'Borders Color', 'ocean-product-sharing' ),
-				'section' => 'ocean_product_sharing_settings',
-				'transport' => 'postMessage',
-				'priority' => 10,
-				'hideLabel' => false,
-				'showAlpha' => true,
-				'showPalette' => true,
+				'type'              => 'ocean-color',
+				'label'             => esc_html__( 'PS Borders', 'ocean-product-sharing' ),
+				'section'           => 'ocean_product_sharing_settings',
+				'transport'         => 'postMessage',
+				'priority'          => 10,
+				'hideLabel'         => false,
+				'showAlpha'         => true,
+				'showPalette'       => true,
 				'sanitize_callback' => 'wp_kses_post',
-				'setting_args' => [
+				'setting_args'      => [
 					'normal' => [
-						'id' => 'ops_product_sharing_borders_color',
-						'key' => 'normal',
-						'label' => esc_html__( 'Select Color', 'ocean-product-sharing' ),
+						'id'       => 'ops_product_sharing_borders_color',
+						'key'      => 'normal',
+						'label'    => esc_html__( 'Select Color', 'ocean-product-sharing' ),
 						'selector' => [
-							'.oew-product-share,.oew-product-share ul li' => 'border-color'
+							'.oew-product-share,.oew-product-share ul li' => 'border-color',
 						],
-						'attr' => [
+						'attr'     => [
 							'transport' => 'postMessage',
 							'default'   => '#e9e9e9',
 						],
@@ -70,24 +77,24 @@ function ops_customizer_options() {
 			],
 
 			'ops_product_sharing_icons_bg' => [
-				'type' => 'ocean-color',
-				'label' => esc_html__( 'Icons Background Color', 'ocean-product-sharing' ),
-				'section' => 'ocean_product_sharing_settings',
-				'transport' => 'postMessage',
-				'priority' => 10,
-				'hideLabel' => false,
-				'showAlpha' => true,
-				'showPalette' => true,
+				'type'              => 'ocean-color',
+				'label'             => esc_html__( 'PS Background', 'ocean-product-sharing' ),
+				'section'           => 'ocean_product_sharing_settings',
+				'transport'         => 'postMessage',
+				'priority'          => 10,
+				'hideLabel'         => false,
+				'showAlpha'         => true,
+				'showPalette'       => true,
 				'sanitize_callback' => 'wp_kses_post',
-				'setting_args' => [
+				'setting_args'      => [
 					'normal' => [
-						'id' => 'ops_product_sharing_icons_bg',
-						'key' => 'normal',
-						'label' => esc_html__( 'Select Color', 'ocean-product-sharing' ),
+						'id'       => 'ops_product_sharing_icons_bg',
+						'key'      => 'normal',
+						'label'    => esc_html__( 'Select Color', 'ocean-product-sharing' ),
 						'selector' => [
-							'.oew-product-share ul li a .ops-icon-wrap' => 'background-color'
+							'.oew-product-share ul li a .ops-icon-wrap' => 'background-color',
 						],
-						'attr' => [
+						'attr'     => [
 							'transport' => 'postMessage',
 							'default'   => '#333333',
 						],
@@ -96,24 +103,24 @@ function ops_customizer_options() {
 			],
 
 			'ops_product_sharing_icons_color' => [
-				'type' => 'ocean-color',
-				'label' => esc_html__( 'Icons Color', 'ocean-product-sharing' ),
-				'section' => 'ocean_product_sharing_settings',
-				'transport' => 'postMessage',
-				'priority' => 10,
-				'hideLabel' => false,
-				'showAlpha' => true,
-				'showPalette' => true,
+				'type'              => 'ocean-color',
+				'label'             => esc_html__( 'PS Icons', 'ocean-product-sharing' ),
+				'section'           => 'ocean_product_sharing_settings',
+				'transport'         => 'postMessage',
+				'priority'          => 10,
+				'hideLabel'         => false,
+				'showAlpha'         => true,
+				'showPalette'       => true,
 				'sanitize_callback' => 'wp_kses_post',
-				'setting_args' => [
+				'setting_args'      => [
 					'normal' => [
-						'id' => 'ops_product_sharing_icons_color',
-						'key' => 'normal',
-						'label' => esc_html__( 'Select Color', 'ocean-product-sharing' ),
+						'id'       => 'ops_product_sharing_icons_color',
+						'key'      => 'normal',
+						'label'    => esc_html__( 'Select Color', 'ocean-product-sharing' ),
 						'selector' => [
-							'.oew-product-share ul li a .ops-icon-wrap .ops-icon' => 'fill'
+							'.oew-product-share ul li a .ops-icon-wrap .ops-icon' => 'fill',
 						],
-						'attr' => [
+						'attr'     => [
 							'transport' => 'postMessage',
 							'default'   => '#ffffff',
 						],
@@ -121,20 +128,13 @@ function ops_customizer_options() {
 				]
 			],
 
-			'ops_divider_for_need_help_link' => [
-				'type' => 'ocean-divider',
-				'section' => 'ocean_product_sharing_settings',
-				'transport' => 'postMessage',
-				'priority' => 10,
-			],
-
 			'ops_need_help_link' => [
-				'type' => 'ocean-content',
-				'isContent' => ocean_render_content_need_help(),
-				'class' => 'need-help',
-				'section' => 'ocean_product_sharing_settings',
+				'type'      => 'ocean-content',
+				'isContent' => sprintf( esc_html__( '%1$s Need Help? %2$s', 'oceanwp' ), '<a href="https://docs.oceanwp.org/article/912-customizer-product-sharing/" target="_blank">', '</a>' ),
+				'class'     => 'need-help',
+				'section'   => 'ocean_product_sharing_settings',
 				'transport' => 'postMessage',
-				'priority' => 10,
+				'priority'  => 10,
 			]
 		]
 	];
